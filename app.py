d9e36753a7d493fccdf3d6cde7ed9317b516eba0
Dev Khant <devkhant24@gmail.com>
2022-10-07 22:03:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 import requests, json
-# from funs import Predictor
+from funs import Predictor
 
-# pred = Predictor('model.onnx')
+pred = Predictor('model.onnx')
 
 app = Flask(__name__)
 
@@ -10,22 +10,22 @@ app = Flask(__name__)
 def home():
     return 'hi there'
 
-# @app.route('/translate', methods=['POST', 'GET'])
-# def translate():
-#     video_path = request.form.get('video') #video path
-# #     video_path = "VN20221006_123507.mp4"
-#     # result = {'pictures': pictures}
+@app.route('/translate', methods=['POST', 'GET'])
+def translate():
+    video_path = request.form.get('video') #video path
+#     video_path = "VN20221006_123507.mp4"
+    # result = {'pictures': pictures}
 
-#     res = json.loads(requests.get(video_path).content.decode("utf-8"))
-#     video_path += "?alt=media&token=" + res["downloadTokens"]
+    res = json.loads(requests.get(video_path).content.decode("utf-8"))
+    video_path += "?alt=media&token=" + res["downloadTokens"]
 
-#     text = pred.predict(video_path)
+    text = pred.predict(video_path)
 
-#     result = {
-#         'word': text
-#     }
+    result = {
+        'word': text
+    }
 
-#     return jsonify(result)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
