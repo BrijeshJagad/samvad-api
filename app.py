461c132e6e28cf6164cf3d361d28efee1474e470
Dev Khant <devkhant24@gmail.com>
2022-10-07 21:39:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,29 @@ from functions import Predictor
 
 app = Flask(__name__)
 
-# pred = Predictor('model.h5')
+pred = Predictor('model.onnx')
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-# @app.route('/translate', methods=['POST', 'GET'])
-# def translate():
-#     # video_path = request.form.get('video') #video path
+@app.route('/translate', methods=['POST', 'GET'])
+def translate():
+    video_path = request.form.get('video') #video path
 #     video_path = "VN20221006_123507.mp4"
-#     # result = {'pictures': pictures}
-#
-#     # res = json.loads(requests.get(video_path).content.decode("utf-8"))
-#     # video_path += "?alt=media&token=" + res["downloadTokens"]
-#
-#     text = pred.predict(video_path)
-#
-#     result = {
-#         'word': text
-#     }
-#
-#     return jsonify(result)
+    # result = {'pictures': pictures}
+
+    res = json.loads(requests.get(video_path).content.decode("utf-8"))
+    video_path += "?alt=media&token=" + res["downloadTokens"]
+
+    text = pred.predict(video_path)
+
+    result = {
+        'word': text
+    }
+
+    return jsonify(result)
 
 
 if __name__ == "__main__":
