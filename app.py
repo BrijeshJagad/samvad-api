e52e9b1dac0e640a713bde309e6f40dfaa53c258
Dev Khant <devkhant24@gmail.com>
2022-10-07 21:53:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return 'hi there'
 
 @app.route('/translate', methods=['POST', 'GET'])
 def translate():
