9a8c25d76f3e8ded2f9c3cca5d00f5d8d83d7fb5
Brijesh Jagad <jagadbrijesh1501@gmail.com>
2022-10-08 14:18:08 +0530

    Update funs.py

--- funs.py
+++ funs.py
@@ -56,7 +56,7 @@ def draw_landmarks(image, results):
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+#     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, face, lh, rh])
@@ -69,7 +69,7 @@ def predict(model, video_path):
     sentence = []
     cap = cv2.VideoCapture(video_path)
 
-    with mp_holistic.Holistic(min_detection_confidence=0.6, min_tracking_confidence=0.6) as holistic:
+    with mp_holistic.Holistic(min_detection_confidence=0.7, min_tracking_confidence=0.7) as holistic:
         while(True):
 
             ret, frame = cap.read()
